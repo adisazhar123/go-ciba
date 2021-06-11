09a6f0682c59aab058eab11afa6cb14f2d46a3f9
adis azhar <adisazhar123@gmail.com>
2021-06-11 21:53:30 +0700

    Add resource server unit test

--- resource_server_test.go
+++ resource_server_test.go
@@ -22,3 +22,66 @@ func TestResourceServer_HandleResourceRequest_ShouldReturnErrInvalidToken(t *tes
 	assert.NotNil(t, err)
 	assert.EqualError(t, err, util.ErrInvalidToken.Error())
 }
+
+func TestResourceServer_HandleResourceRequest_ShouldReturnErrInsufficientScope(t *testing.T) {
+	rs := &ResourceServer{
+		accessTokenRepo: test_data.NewAccessTokenVolatileRepository(),
+		scopeUtil:       util.ScopeUtil{},
+	}
+	token := "C59D9FBC-D8E4-4B8B-A95B-14F931EE1AB3"
+
+	err := rs.HandleResourceRequest(&ResourceRequest{accessToken: token}, "payment:write")
+
+	assert.NotNil(t, err)
+	assert.EqualError(t, err, util.ErrInsufficientScope.Error())
+}
+
+func TestResourceServer_HandleResourceRequest_ShouldReturnErrInsufficientScope2(t *testing.T) {
+	rs := &ResourceServer{
+		accessTokenRepo: test_data.NewAccessTokenVolatileRepository(),
+		scopeUtil:       util.ScopeUtil{},
+	}
+	token := test_data.AccessTokenValid.Value
+
+	err := rs.HandleResourceRequest(&ResourceRequest{accessToken: token}, "openid email profile chat:write payment:write")
+
+	assert.NotNil(t, err)
+	assert.EqualError(t, err, util.ErrInsufficientScope.Error())
+}
+
+func TestResourceServer_HandleResourceRequest_ShouldReturnErrInvalidTokenWhenExpired(t *testing.T) {
+	rs := &ResourceServer{
+		accessTokenRepo: test_data.NewAccessTokenVolatileRepository(),
+		scopeUtil:       util.ScopeUtil{},
+	}
+	token := test_data.AccessTokenExpired.Value
+
+	err := rs.HandleResourceRequest(&ResourceRequest{accessToken: token}, "")
+
+	assert.NotNil(t, err)
+	assert.EqualError(t, err, util.ErrInvalidToken.Error())
+}
+
+func TestResourceServer_HandleResourceRequest_ShouldSucceedWhenTokenIsValid(t *testing.T) {
+	rs := &ResourceServer{
+		accessTokenRepo: test_data.NewAccessTokenVolatileRepository(),
+		scopeUtil:       util.ScopeUtil{},
+	}
+	token := test_data.AccessTokenValid.Value
+
+	err := rs.HandleResourceRequest(&ResourceRequest{accessToken: token}, "")
+
+	assert.Nil(t, err)
+}
+
+func TestResourceServer_HandleResourceRequest_ShouldSucceedWhenTokenIsValid_CustomScope(t *testing.T) {
+	rs := &ResourceServer{
+		accessTokenRepo: test_data.NewAccessTokenVolatileRepository(),
+		scopeUtil:       util.ScopeUtil{},
+	}
+	token := test_data.AccessTokenValid.Value
+
+	err := rs.HandleResourceRequest(&ResourceRequest{accessToken: token}, "chat:write")
+
+	assert.Nil(t, err)
+}
